44d2198022d4f1f484865681663b261ce179f1de
caio travain <caiotravain@gmail.com>
2021-05-19 16:18:00 -0300

    one

---
+++ jogo.py
@@ -0,0 +1,35 @@
+import pygame
+import random
+import time
+
+pygame.init()
+pygame.mixer.init()
+
+# ----- Gera tela principal
+WIDTH = 480
+HEIGHT = 600
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Stick')
+
+# ----- Inicia assets
+stick_img = pygame.image.load('pygame/assets/img/stick.png').convert_alpha()
+
+#Loop principal
+game = True
+while game:
+    # ----- Trata eventos
+    for event in pygame.event.get():
+        # ----- Verifica consequências
+        if event.type == pygame.QUIT:
+            game = False
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_c:
+                game=False
+    # ----- Gera saídas
+    window.fill((255, 255, 255))  # Preenche com a cor branca
+
+    # ----- Atualiza estado do jogo
+    pygame.display.update()  # Mostra o novo frame para o jogador
+
+# ===== Finalização =====
+pygame.quit()  # Função do PyGame que finaliza os recursos utilizados
