573c18dacc38e6a1b1b1bb5afc396883f8607f0f
Felipe Trintim <felipemt3@al.insper.edu.br>
2021-05-22 19:59:52 -0300

    update

--- jogo.py
+++ jogo.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 import time
+from pygame.locals import *
 
 pygame.init()
 
@@ -11,11 +12,17 @@ window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Stick')
 stick_largura = 30
 stick_altura = 60
+mira_largura = 200
+mira_altura = 200
+
 # ----- Inicia assets
 background1 = pygame.image.load('pygame/assets/img/fase1.jpg').convert_alpha()
 stick_img = pygame.image.load('pygame/assets/img/stick_branco.png').convert_alpha()
+mira = pygame.image.load('pygame/assets/img/mira.png').convert_alpha()
 stick_img = pygame.transform.scale(stick_img, (stick_largura, stick_altura))
+mira_img = pygame.transform.scale(mira, (mira_largura, mira_altura))
 stick_inv = pygame.transform.flip(stick_img, True, False)
+
 class stick(pygame.sprite.Sprite):
     def __init__(self,img, x, y):
         pygame.sprite.Sprite.__init__(self)
@@ -42,12 +49,16 @@ class stick(pygame.sprite.Sprite):
 stick1 = stick(stick_img, 200, 215)
 stick2 = stick(stick_img, 0, 128)
 stick3 = stick(stick_img,850,160)
+
 #Loop principal
 clock = pygame.time.Clock()
 FPS = 30
 game = True
+
 while game:
     clock.tick(FPS)
+    # mx, my = pygame.mouse.get_pos()
+    
     # ----- Trata eventos
     for event in pygame.event.get():
         # ----- Verifica consequências
@@ -56,15 +67,25 @@ while game:
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_c:
                 game=False
+
+    
     stick1.update(150, 450)
     stick2.update(0, 110)
     stick3.update(850,960 - stick_largura)
+    
     # ----- Gera saídas
     window.fill((255, 255, 255))  # Preenche com a cor branca
     window.blit(background1, (0,0))
     window.blit(stick1.image, stick1.rect)
     window.blit(stick2.image, stick2.rect)
     window.blit(stick3.image,stick3.rect)
+
+    mousePos = pygame.mouse.get_pos()
+    mira_X = pygame.mouse.get_pos()[0] - 50
+    mira_Y = pygame.mouse.get_pos()[1] - 50
+    mira_jogo = window.blit(mira_img, (mira_X, mira_Y))
+
+    
     # ----- Atualiza estado do jogo
     pygame.display.update()  # Mostra o novo frame para o jogador
 
