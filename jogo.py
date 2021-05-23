454e7991f0a4d3f75363c665d3a4ead93b8aaf29
Gabriel Miné <gabrielzm1@al.insper.edu.br>
2021-05-23 13:02:38 -0300

    update

--- jogo.py
+++ jogo.py
@@ -27,29 +27,43 @@ stick_inv = pygame.transform.flip(stick_img, True, False)
 bolinha = pygame.image.load('pygame/assets/img/bolinha.png').convert_alpha()
 bolinha_img = pygame.transform.scale(bolinha, (5, 5))
 class stick(pygame.sprite.Sprite):
-    def __init__(self,img, x, y):
+    def __init__(self,img, x, y,nivel):
         pygame.sprite.Sprite.__init__(self)
         self.image = img
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
-        self.speedx = (1.5)
         self.speedy = (0)
+        if nivel == 1:
+            self.speedx = (1.5)
+        elif nivel == 2:
+            self.speedx = (3)
 
     def update(self, x_min, x_max,a):
         # Atualizando a posição do stick
         self.rect.x += self.speedx
         self.rect.y += self.speedy
-        if a ==2 :
-            self.rect.x = 10000
-            self.speedx = 0
-        if self.rect.x >= x_max:
-            self.speedx = (-1) 
-            self.image = stick_inv           
-        if self.rect.x <= x_min:
-            self.speedx = (1.5)
-            self.image = stick_img
-        # Armazena a animação de explosão
+        if nivel == 1:
+            if a == 2:
+                self.rect.x = 10000
+                self.speedx = 0
+            if self.rect.x >= x_max:
+                self.speedx = (-1) 
+                self.image = stick_inv           
+            if self.rect.x <= x_min:
+                self.speedx = (1.5)
+                self.image = stick_img
+        if nivel == 2:
+            if a == 2:
+                self.rect.x = 10000
+                self.speedx = 0
+            if self.rect.x >= x_max:
+                self.speedx = (-2.5) 
+                self.image = stick_inv           
+            if self.rect.x <= x_min:
+                self.speedx = (3)
+                self.image = stick_img
+
 class mira(pygame.sprite.Sprite):
     def __init__(self, img, x, y):
         # Construtor da classe mãe (Sprite).
@@ -76,25 +90,26 @@ class bolinha(pygame.sprite.Sprite):
         self.rect.y = y
 
 #Criando Sticks
-stick1 = stick(stick_img, 200, 215)
-stick2 = stick(stick_img, 0, 128)
-stick3 = stick(stick_img,850,160)
-stick4 = stick(stick_img,280,43)
-stick5 = stick(stick_img,540,246)
-stick6 =stick(stick_img,740,130)
-stick7 =stick(stick_img,100,282)
-stick8 =stick(stick_img,360,192)
+stick1 = stick(stick_img, 200, 215,1)
+stick2 = stick(stick_img, 0, 128,1)
+stick3 = stick(stick_img,850,160,1)
+stick4 = stick(stick_img,280,43,2)
+stick5 = stick(stick_img,540,246,2)
+stick6 =stick(stick_img,740,130,2)
+stick7 =stick(stick_img,100,282,2)
+stick8 =stick(stick_img,360,192,2)
 mira1 = mira(mira_img,0,0)
 bolinha1 = bolinha(bolinha_img,0,0)
 all_sticks = pygame.sprite.Group()
 groups = {}
 groups['all_sticks'] = all_sticks
+lista_stick = [stick1,stick2,stick3,stick4,stick5,stick6,stick7,stick8]
 #Loop principal
 clock = pygame.time.Clock()
 FPS = 30
 game = True
 abatido = 0
-nivel = 2
+nivel = 1
 while game:
     clock.tick(FPS)
     # mx, my = pygame.mouse.get_pos()
@@ -109,20 +124,11 @@ while game:
                 game=False
         pygame.mouse.set_visible(False)
         if event.type == pygame.MOUSEBUTTONDOWN:
-            hit_stick_1 =pygame.sprite.collide_rect(bolinha1, stick1)
-            if hit_stick_1 == 1:
-                stick1.update(0,0,2)
-                abatido += 1
-            hit_stick_2 =pygame.sprite.collide_rect(bolinha1, stick2)
-            if hit_stick_2 == 1:
-                stick2.update(0,0,2)
-                abatido +=1
-            hit_stick_3 =pygame.sprite.collide_rect(bolinha1, stick3)
-            if hit_stick_3 == 1:
-                abatido +=1
-                stick3.update(0,0,2)
-
-        
+            for a in lista_stick:
+                hit = pygame.sprite.collide_rect(bolinha1,a)
+                if hit == 1:
+                    a.update(0,0,2)
+                    abatido += 1
         
     mousePos = pygame.mouse.get_pos()
     mouse_x_b = pygame.mouse.get_pos()[0]-2.5
