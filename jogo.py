e13b37060ee5e342ca228b0f76ab334f248d59f9
Felipe Trintim <felipemt3@al.insper.edu.br>
2021-06-02 16:07:30 -0300

    update

--- jogo.py
+++ jogo.py
@@ -1,3 +1,4 @@
+from os import times
 import pygame
 import time
 from pygame.locals import *
@@ -24,6 +25,7 @@ fonte3 = pygame.font.Font("pygame/assets/fontes/8-bit.ttf", 30)
 font_sys = pygame.font.SysFont(None, 30)
 texto = fonte2.render('Press Any Button to Start', True, (255, 255, 255))
 texto3 = fonte2.render('Score', True, (255, 255, 255))
+texto4 = fonte2.render('Time', True, (255, 255, 255))
 background = pygame.image.load('pygame/assets/img/fase1.jpg').convert_alpha()
 background2 = pygame.image.load('pygame/assets/img/fase2.jpg').convert_alpha()
 background2 = pygame.transform.scale(background2, (WIDTH, HEIGHT))
@@ -33,6 +35,7 @@ Sobre = pygame.image.load('pygame/assets/img/sobreposicao.png').convert_alpha()
 sobre = pygame.transform.scale(Sobre, (385, 430))
 score = pygame.image.load('pygame/assets/img/score.png').convert_alpha()
 score = pygame.transform.scale(score, (150,60))
+times = pygame.transform.scale(score, (120,30))
 j1 = pygame.image.load('pygame/assets/img/janela1.png').convert_alpha()
 j1 = pygame.transform.scale(j1, (65, 60))
 j2 = pygame.image.load('pygame/assets/img/janela2.png').convert_alpha()
@@ -342,7 +345,9 @@ while game:
         window.blit(score, ((WIDTH/2) - 70,30))
         window.blit(pontos, ((WIDTH/2) - 40, 44))
         window.blit(texto3,((WIDTH/2) - 45, 5))
-        window.blit(tempo,(800, 5))
+        window.blit(times, (800,30))
+        window.blit(texto4,(815, 5))
+        window.blit(tempo,(830, 37))
         
         
         if aparecer == True:
